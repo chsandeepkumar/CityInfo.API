4b03e98af9bba5603b1666fbc4e6aed442559e69
Schintalapally <Schintalapally@egov.com>
2019-02-22 10:31:39 -0600

    [Changelist-1] updated the Patch and Delete methods

--- CityInfo.API/Model/PointOfInterest.cs
+++ CityInfo.API/Model/PointOfInterest.cs
@@ -1,8 +1,13 @@
-﻿namespace CityInfo.API.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Model
 {
     public class PointOfInterest
     {
+        [Required(ErrorMessage = "Provide a Name filed value")]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(50)]
         public string Description { get; set; }
     }
 }
